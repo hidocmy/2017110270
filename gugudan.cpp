1cb855b0404eafc0404135621a4c1dce3c649327
hidocmy <hidocmy@khu.ac.kr>
2021-10-17 18:51:45 +0900

    구구단을 구구단으로 풀리퀘스트 과제

--- gugudan.cpp
+++ gugudan.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 int main() {
+    cout<<"구구단 업데이트"<<endl;
     for (int i = 1; i <= 9; i++) {
-        for (int j = 2; j <= 9; j++)  //8À» 9·Î ¼öÁ¤
+        for (int j = 1; j <= 9; j++)  //8À» 9·Î ¼öÁ¤
             cout << j << " x " << i << " = " << j * i << "\t";
         cout << endl;
     }
